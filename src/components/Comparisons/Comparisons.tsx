7318dbe01cbf395f3804ac3b6444198bae613c56
Alexander Wang <alex@terrastruct.com>
2022-09-29 20:06:31 -0700

    2022-09-29 08:06:31PM

--- src/components/Comparisons/Comparisons.tsx
+++ src/components/Comparisons/Comparisons.tsx
@@ -160,7 +160,7 @@ function Langs(props: LangsProps) {
               </div>
 
               <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
               </svg>
             </div>
           </button>
