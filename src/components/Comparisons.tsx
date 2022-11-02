cb83f93f056831e3f818945284519f52e55eb626
Bernard Xie <bernard@terrastruct.com>
2022-11-02 10:33:31 -0700

    mobile comparison switches

--- src/components/Comparisons.tsx
+++ src/components/Comparisons.tsx
@@ -358,12 +358,7 @@ function Comparison(props: ComparisonProps) {
 
     return (
       <div className='relative flex w-full items-center justify-center'>
-        <embed
-          width='100%'
-          src={src}
-          className=''
-          // alt={`Example of ${props.lang}`}
-        />
+        <img width='100%' src={src} className='' alt={props.name} />
       </div>
     );
   };
