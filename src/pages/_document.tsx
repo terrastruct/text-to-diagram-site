3e91312adb24813eb9ba19a6d8ad87a66d83049f
Alexander Wang <alex@terrastruct.com>
2022-12-19 16:46:37 -0800

    remove nonexistant header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,13 +10,6 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link
-            rel='preload'
-            href='/fonts/inter-var-latin.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymous'
-          />
         </Head>
         <body>
           <Main />
