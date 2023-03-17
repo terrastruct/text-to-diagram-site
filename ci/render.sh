4421b7e543a2dbfc8d1f99424b73c32de382ba96
Alexander Wang <alex@terrastruct.com>
2023-03-17 14:13:04 -0700

    update to latest

--- ci/render.sh
+++ ci/render.sh
@@ -12,7 +12,7 @@ _d2() {
 mmdc() {
   config=$(mktempd)/mmdc-config
   echo '{"deterministicIds": true}' >$config
-  npx --package @mermaid-js/mermaid-cli@9.2.2 -- mmdc -c"$config" "$@"
+  npx --package @mermaid-js/mermaid-cli@10.0.2 -- mmdc -c"$config" "$@"
 }
 
 _mmdc() {
