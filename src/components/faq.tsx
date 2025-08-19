123b60a98c13f289e05fef10623ef65a4d7fb319
Alexander Wang <alex@terrastruct.com>
2025-08-19 12:51:26 -0600

    Regenerate d2

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -37,7 +37,7 @@ const items = [
   },
   {
     question: 'How up-to-date is this?',
-    answer: `Very up to date. Every so often, we upgrade to latest version of all tools and regenerate all. Latest: August 12, 2025.`,
+    answer: `Very up to date. Every so often, we upgrade to latest version of all tools and regenerate all. Latest: August 19, 2025.`,
   },
   {
     question: 'Who made this?',
